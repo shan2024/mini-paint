2c1b4795b39ae191f04d51111a492dabdd39fd90
Seulchan Han <paulh27@uw.edu>
2021-09-14 15:11:27 -0700

    working version, fix optimization

--- paint.js
+++ paint.js
@@ -11,16 +11,51 @@ createGridUnit(4);
 //slider functionality
 let gridSize = document.querySelector(".left p");
 let slider = document.querySelector(".slider");
-console.log(slider.value);
+let rainbow = document.querySelector(".rainbow");
+let eraser = document.querySelector(".eraser");
+let clear = document.querySelector(".clear");
+let colorPicker = document.querySelector("#colorpicker");
+
+let backgroundColor = "black";
+let rainbowMode = false;
+
+colorPicker.onclick = function() {
+    rainbowMode = false;
+    backgroundColor = this.value;
+}
+
+colorPicker.onchange = function () {
+    rainbowMode = false;
+    backgroundColor = this.value;
+}
+
+eraser.onclick = function () {
+    rainbowMode = false;
+    backgroundColor = "white";
+}
+
+clear.onclick = function () {
+    let gridItems = Array.from(document.querySelectorAll(".unit"));
+
+    gridItems.forEach((item) => {
+
+        item.style.backgroundColor = "white";
+    })
+}
+
+rainbow.onclick = function () {
+    rainbowMode = true;
+}
+
 
 //slider.addEventListener("oninput", updateGrid);
-slider.oninput = function() {
+slider.oninput = function () {
     console.log(this.value);
-    gridSize.innerHTML = this.value + "x" + this.value;
+    gridSize.innerHTML = this.value + " x " + this.value;
     createGridUnit(this.value);
 
 
-  }
+}
 
 function createGridUnit(size) {
     while (center.firstChild) {
@@ -32,41 +67,44 @@ function createGridUnit(size) {
     }
     center.style.gridTemplateColumns = gridInput;
     center.style.gridTemplateRows = gridInput;
-    for (let i = 0; i < Math.pow(size,2); i++) {
+
+    for (let i = 0; i < Math.pow(size, 2); i++) {
         let div = document.createElement("div");
         div.setAttribute("class", "unit");
         div.style.width = `${center.offsetWidth / size}px`;
         div.style.height = div.style.width;
-        //console.log(`${center.offsetWidth / size}`);
         center.appendChild(div);
 
     }
     let gridItems = Array.from(document.querySelectorAll(".unit"));
 
     gridItems.forEach((item) => {
-    console.log("adding events");
 
-    item.addEventListener("mouseenter", color);
-})
-}
-function updateGrid(size) {
-    console.log(size);
-    gridSize.innerHTML = "Grid Size: " + size + "x" + size;
-    createGridUnit(size);
+        item.addEventListener("mouseenter", hoverColor);
+    })
 }
 
 
-function color(e) {
-    console.log("hovering");
+
+function hoverColor(e) {
     let unit = e.target;
-    unit.classList.remove("clear");
-    unit.classList.add("hovering");
+    if (rainbowMode == true) {
+        let rand1 = Math.random() * 255;
+        let rand2 = Math.random() * 255;
+        let rand3 = Math.random() * 255;
+        unit.style.backgroundColor = `rgb(${rand1}, ${rand2}, ${rand3})`;
+    }
+    else {
+        unit.style.backgroundColor = backgroundColor;
+
+    }
 }
 
-function clear() {
-    console.log("clear");
+function inProgress() {
     gridItems.forEach((item) => {
         item.classList.remove("hovering");
         item.classList.add("clear");
     })
 }
+
+
